fd2ae19b1011a8b7b35c747dd2d6e4ab2fb976a0
marmejia <maria.mejia@mercadolibre.com.co>
2022-10-21 15:11:48 -0500

    Ultimo commit

--- models/devuelvoTweetsSeguidores.go
+++ models/devuelvoTweetsSeguidores.go
@@ -9,7 +9,7 @@ type DevuelvoTweetsSeguidores struct {
 	ID               primitive.ObjectID `bson:"_id" json:"_id,omitempty"`
 	UsuarioID        string             `bson:"usuarioId" json:"usuarioId,omitempty"`
 	UsuarioRelacioID string             `bson:"usuarioRelacionId" json:"usuarioRelacionId,omitempty"`
-	Twitter          struct {
+	Tweet            struct {
 		Mensaje string    `bson:"mensaje" json:"mensaje,omitempty"`
 		Fecha   time.Time `bson:"fecha" json:"fecha,omitempty"`
 		ID      string    `bson:"_id" json:"_id,omitempty"`
